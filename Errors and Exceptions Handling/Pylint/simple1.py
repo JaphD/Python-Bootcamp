7f9db9caad5d216d72a20124d4dc944018eece85
Yafet Daniel Haileselassie <japhethdanny@gmail.com>
2024-08-26 17:50:24 +0300

    Update
    Tested pylint library

---
+++ Errors and Exceptions Handling/Pylint/simple1.py
@@ -0,0 +1,17 @@
+"""
+A simple python script for testing out pylint
+"""
+
+
+def myfunc():
+    """
+    A simple function
+    """
+    first = 1
+    second = 2
+
+    print(first)
+    print(second)
+
+
+myfunc()
